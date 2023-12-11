1b80a8bde5b6280c37e01a0670c3039fbe4e913a
Nate Sobeck <nsobeck910@gmail.com>
2023-12-11 10:51:33 -0500

    display inventory

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,7 +1,18 @@
+// data
+import { inventoryData } from "../../data/data"
+
 const Shop = () => {
   return ( 
     <main>
       <h1>Shop</h1>
+      <ul>
+        {inventoryData.map(item => (
+          <li key={item.id}>
+            <p>{item.name}</p>
+            <p>Cost: {item.cost}</p>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
